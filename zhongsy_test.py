85cb561b99eca3dba77ae95b57409ea35d0dc5c8
xiaohao-a <xuxinhaoim@163.com>
2021-05-12 18:58:48 +0800

    add save to mongo

--- zhongsy_test.py
+++ zhongsy_test.py
@@ -20,6 +20,9 @@ class ZhongSY:
         }
         self.public_key = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCfXfMzgg4m5RRLg2vcrYBFN4sBhE1VtW1sBkXxC5wtCRaOZv0kudk9CIQfU6c+eEaaZKUnygxHWdSqdwURCE0IKgLcolXF+RHmu/rl977FfjRg9pAkBg5z05PfHDqWqkIsqX0iRaSP31BUZOgtwafbiBv2dBvRBMdq03ty4q8OQQIDAQAB"
         self.private_Key = "MIICdQIBADANBgkqhkiG9w0BAQEFAASCAl8wggJbAgEAAoGBAITfvlC8+Nr+vz3DnhuCWW41ax8PG+rCiXt/f4XjRMlj9ZC2AuMMbtHLsTMLhCrhgHt1MxdcoYtqvQfxu4AVOh6pZrxMr2AiyNpw8SecmM3m0YWYNc7tnUB6/vlLyQduikD4qaxNiB5FcUiRpiRoLpz7rT6UV+/zDh+ibgvZRLDRAgMBAAECgYB7/mMV6tJ7YkBKPdK8Lw6PZq/5Att1XmZ3ZYo2Adg96tbMXN0izYZYprFMRhHnBhokm0C7K0jg1hFiaXUkWCqr83H+Y+DZ7js9NDhApPYAELQDIu288/nz34mjU/wnIGWP6WK5PCd1QjR8ltFay1TDLecdavHHjWGfHOMYnY5/dQJBAOZ4ICB+VrXMwR8KUR3r420YAHPwQDQKDetMHwgYHtFUH/k3CtKzPrltx103OhQcKyfrkoPj8SREZZISaBEQL6cCQQCTl+pjOSMud4hFTvfTnkGx9EZT3dBAv31ZfzHCu4g41FxRLyJLY6iKce069IhMjC2gfoLtwDLM/dKzRAuw9+rHAkAd9/zlfMg1t7xdFvBZXbUjGH3mlZUjrzMEJ8/ZM5m+SpwlwfyMTXaYkifcfTP2LXuHI2DX+an/t00l43LY1Sv9AkAEgQ5WGNhKArvV4aMOgjXfCGVdCdfhIfbhVFBgcPinQ1PN5nJVeqUaFH/43J2MOHrr+vBj8Qmb1+MmNV1l+SrhAkArJjCosjMI32RT3GmC6+gwxADR9Ib53yDHwRoMeO34dgK3hj3+e66Jhpcht3AjXBVs7bF9xzXcePpxxCka9cEv"
+        self.conn = pymongo.MongoClient('192.168.31.63',27017)
+        self.db = self.conn['zhongsy']
+        self.set = self.db['bidding']
 
     def page_data(self,max_page):
         """ 页面申请原文生成器，生成请求信息的json字符串 """
@@ -74,10 +77,10 @@ class ZhongSY:
         return response.json()
 
     def save_data(self,data):
-        print(data)
+        self.set.insert_many(data)
 
     def run(self):
-        page_data = self.page_data(3)
+        page_data = self.page_data(10)
         for msg in page_data:
             msg_str = json.dumps(msg)
             random_str = self.random_str(16)
@@ -92,6 +95,7 @@ class ZhongSY:
             print('成功解密数据')
             result_json = json.loads(result)
             information_list = result_json['list']
+            print(information_list)
             self.save_data(information_list)
 
 if __name__ == '__main__':
